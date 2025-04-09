5c2f6f24ba6a65ea9fcd9485a86e0a40ee90b2a4
ElieOko <elieoko100@gmail.com>
2025-04-09 13:14:11 -0700

    fix:new constant

--- app/src/main/java/com/partners/hdfils_agent/data/utils/Constants.kt
+++ app/src/main/java/com/partners/hdfils_agent/data/utils/Constants.kt
@@ -1,5 +1,12 @@
 package com.partners.hdfils_agent.data.utils
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.ui.res.painterResource
+import com.partners.hdfils_agent.domain.models.Route
+import com.partners.hdfils_agent.R
 
 class Constants {
     companion object {
@@ -8,6 +15,12 @@ class Constants {
         const val HOST_PROD = "127.0.0.1"
         const val IS_PROD = false
         const val TokenLocal = ""
+        val routeList = listOf(
+            Route(R.drawable.home, title = "Acceuil", routeName = "home"),
+            Route(R.drawable.user_people, title = "Client", routeName = "client"),
+            Route(R.drawable.trash_clean, title = "Vidage", routeName = "vidage"),
+            Route(R.drawable.user_, title = "Profil", routeName = "profil")
+        )
         const val API_INTERNET_MESSAGE="No Internet Connection"
         val listGenre = listOf("Mr(M)", "Mme(F)")
         val listVille = listOf("Kinshasa", "Boma", "Matadi")
