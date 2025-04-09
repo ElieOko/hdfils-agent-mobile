619bbce8470767bcc206d9f7f8f5c00c6380cec0
ElieOko <elieoko100@gmail.com>
2025-04-09 13:16:08 -0700

    fix:new package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
     //Coroutines
     implementation (libs.kotlinx.coroutines.core)
     implementation (libs.kotlinx.coroutines.android)
