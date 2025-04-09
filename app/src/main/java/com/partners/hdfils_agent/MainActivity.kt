2b436e09e47e04cf2bc83e6a81da6647aa96ff1d
ElieOko <elieoko100@gmail.com>
2025-04-08 23:20:53 -0700

    fix:call components

--- app/src/main/java/com/partners/hdfils_agent/MainActivity.kt
+++ app/src/main/java/com/partners/hdfils_agent/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.partners.hdfils_agent
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,19 +12,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.partners.hdfils_agent.presentation.ui.pages.AuthPage
 import com.partners.hdfils_agent.presentation.ui.theme.HdfilsagentTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             HdfilsagentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    AuthPage()
                 }
             }
         }
